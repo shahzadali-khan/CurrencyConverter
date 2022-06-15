80870f36075a619c8d78aacbfcbd2db0518e0505
Shahzad Ali Khan <shahzadali.khan@convenegroup.com>
2022-06-15 13:18:06 +0200

    blazor page edited

--- Netwealth/src/Client/Services/CurrencyConverterService.cs
+++ Netwealth/src/Client/Services/CurrencyConverterService.cs
@@ -1,15 +1,32 @@
-﻿using Shared;
+﻿using System.Net.Http.Json;
+using Shared;
 
 namespace Client.Services
 {
-    public class CurrencyConverterService: ICurrencyConverterService
+    public class CurrencyConverterService : ICurrencyConverterService
     {
-        public Task<CurrencyConverterDto> GetCurrencyConverted(string @from, string to, decimal amount)
+        private readonly HttpClient _client;
+
+
+        public CurrencyConverterService(HttpClient client)
         {
-            throw new NotImplementedException();
+            _client = client;
+        }
+
+        public async Task<CurrencyConverterDto?> GetCurrencyConverted(string @from, string to, decimal amount)
+        {
+            try
+            {
+                return await _client.GetFromJsonAsync<CurrencyConverterDto>($"/api/{from}&{to}&{amount}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(@"Error fetching currency rates : " + e.Message);
+            }
+
+            return null;
         }
     }
-
     public interface ICurrencyConverterService
     {
         Task<CurrencyConverterDto> GetCurrencyConverted(string from, string to, decimal amount);
